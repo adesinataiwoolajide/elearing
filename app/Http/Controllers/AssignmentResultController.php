f7b8ca65bedbd944538a6c7039b39000b7633fc3
adesinataiwoolajide <tolajide74@gmail.com>
2019-08-28 18:02:28 +0100

    first commit

---
+++ app/Http/Controllers/AssignmentResultController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;;
+use Spatie\Permission\Models\Role;
+use App\{Courses, CourseAllocations, User, Staffs, Assignments, Students, AssignmentResult};
+use DB;
+use Illuminate\Support\Facades\Auth;
+use App\Repositories\AssignmentResultRepository;
+use Illuminate\Support\Facades\Gate;
+
+class AssignmentResultController extends Controller
+{
+    protected $model;
+    public function __construct(AssignmentResult $result)
+    {
+       // set the model
+       $this->model = new AssignmentResulttRepository($result);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/administrator/submissions/index.blade.php
@@ -0,0 +1,152 @@
+@extends('layouts.app')
+    @section('content')
+
+    <div class="dt-content-wrapper custom-scrollbar">
+
+        <!-- Site Content -->
+        <div class="dt-content">
+
+            <div class="row">
+
+                <!-- Grid Item -->
+                <div class="col-12">
+                    <div class="row dt-masonry">
+                        <div class="col-md-12 dt-masonry__item">
+                            <div class="dt-card">
+
+                                <ol class="mb-0 breadcrumb">
+                                    <li class="breadcrumb-item"><a href="{{route('administrator.dashboard')}}">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="{{route('submission.index')}}">View Submission</a></li>
+
+                                    {{-- <li class="breadcrumb-item"><a href="{{route('solution.create')}}">View Course solution</a></li> --}}
+
+                                    {{-- @if (auth()->user()->hasPermissionTo('Restore Assignment') OR
+                                        (Gate::allows('Administrator', auth()->user())))
+                                        <li class="breadcrumb-item"><a href="{{route('assignment.restore')}}">Restore Deleted Assignment</a></li>
+                                    @endif
+
+                                    @if (auth()->user()->hasRole('Staff'))
+                                        <li class="breadcrumb-item active" aria-current="page">{{auth()->user()->name}} Assignment List </li>
+                                    @else
+                                        <li class="breadcrumb-item active" aria-current="page">List of Course Assignment </li>
+                                    @endif --}}
+                                </ol>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="dt-card">
+
+                            <!-- Card Body -->
+                            <div class="dt-card__body">
+                                @if(count($solution) ==0)
+                                    <p align="center" style="color: red"><i class="icon icon-table"></i>
+                                        The Solution List is Empty
+                                    </p>
+
+                                @else
+                                    <!-- Tables -->
+                                    <div class="table-responsive">
+
+                                        <table id="data-table" class="table table-striped table-bordered table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th>S/N </th>
+                                                    <th>Student Matric</th>
+                                                    <th>Course Title</th>
+                                                    <th>Question</th>
+                                                    <th>Mark</th>
+                                                    <th>Submission Date</th>
+                                                    <th>Lecturer</th>
+                                                    <th>Program</th>
+                                                    <th>Level</th>
+                                                </tr>
+                                            </thead>
+
+                                            <tbody>
+                                                    <?php
+                                                $y=1; ?>
+                                                @foreach ($solution as $solutions)
+                                                    <tr class="gradeX">
+                                                        <td>{{$y}}
+                                                            {{-- @if (auth()->user()->hasPermissionTo('Delete Assignment') OR
+                                                                (Gate::allows('Administrator', auth()->user())))
+                                                                <a href="{{route('assignment.delete', $solutions->assignment_id)}}"
+                                                                    class=""
+                                                                    onclick="return(confirmToDelete());" >
+                                                                <i class="fa fa-trash-o"></i></a>
+                                                            @endif
+                                                            @if (auth()->user()->hasRole('Staff'))
+                                                                <a href="{{route('assignment.edit', $solutions->assignment_id)}}"
+                                                                    class="" onclick="">
+                                                                    <i class="fa fa-pencil"></i>
+                                                                </a>
+                                                                <a href="{{route('assignment.edit', $solutions->assignment_id)}}"
+                                                                    class="" onclick="">
+                                                                    <i class="fa fa-list"></i>
+                                                                </a>
+                                                            @endif
+                                                            @if (auth()->user()->hasRole('Student'))
+                                                                <a href="{{route('assignment.solution', $solutions->assignment_id)}}"
+                                                                    class="" onclick="">
+                                                                    <i class="fa fa-book"></i>
+                                                                </a>
+                                                            @endif --}}
+                                                        </td>
+                                                        <td>{{$solutions->student->matric_number}}</td>
+                                                        {{-- <td>{{$solutions->course->course_title}}</td>
+                                                        <td>{{$solutions->question}}</td>
+                                                        <td>{{$solutions->marks}}</td>
+                                                        <td>{{$solutions->submission_date}}</td>
+                                                        <td>{{$solutions->user->name}}</td>
+                                                        <td>{{$solutions->program}}</td>
+                                                        <td>{{$solutions->level}}</td> --}}
+
+                                                    </tr><?php
+                                                    $y++; ?>
+
+                                                @endforeach
+
+                                            </tbody>
+
+                                            <tfoot>
+                                                <tr>
+                                                    <tr>
+                                                        <th>S/N </th>
+                                                        <th>Course Title</th>
+                                                        <th>Question</th>
+                                                        <th>Mark</th>
+                                                        <th>Submission Date</th>
+                                                        <th>Lecturer</th>
+                                                        <th>Program</th>
+                                                        <th>Level</th>
+
+                                                    </tr>
+                                                </tr>
+                                            </tfoot>
+                                        </table>
+
+                                    </div>
+                                    <!-- /tables -->
+                                @endif
+                            </div>
+                            <!-- /card body -->
+
+                        </div>
+
+
+                </div>
+
+
+            </div>
+        </div>
+        <!-- /site content -->
+
+        <!-- Footer -->
+        <footer class="dt-footer">
+           <a href=""> Powered By Glorious Empire Technologies </a> © {{date('Y')}}
+        </footer>
+<!-- /footer -->
+</div>
+@endsection
